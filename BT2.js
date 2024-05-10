17667b21f819463e107901b2229c5fa0e732087d
levanhieu2122110522 <lehieu1542004@gmail.com>
2024-05-10 23:36:59 +0700

    buoi 01

---
+++ BT2.js
@@ -0,0 +1,14 @@
+document.getElementById("show").innerHTML =`
+<tr>
+    <th>STT</th>
+    <th>MaSv</th>
+    <th>HoTen</th>
+    <th>Lop</th>
+</tr>
+<tr>
+    <th>STT</th>
+    <th>MaSv</th>
+    <th>HoTen</th>
+    <th>Lop</th>
+</tr>
+`
